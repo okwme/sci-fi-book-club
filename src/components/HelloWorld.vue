24109639c667d9c4890bd26a0ac0eba80d13a9fe
Billy Rennekamp <billy.rennekamp@gmail.com>
2018-08-15 11:11:59 +0200

    add newsletter

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -15,6 +15,28 @@
       <div v-if="serverError">{{serverError}}</div>
       <div v-if="sucessfulServerResponse">{{sucessfulServerResponse}}</div>
     </form>
+
+
+    <!-- Begin MailChimp Signup Form -->
+<div id="mc_embed_signup">
+<form action="https://club.us18.list-manage.com/subscribe/post?u=6da71990ae231989fb097187b&amp;id=a1f732c8eb" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+    <div id="mc_embed_signup_scroll">
+
+<div class="mc-field-group">
+	<label for="mce-EMAIL">Join The Newsletter: </label>
+	<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+</div>
+	<div id="mce-responses" class="clear">
+		<div class="response" id="mce-error-response" style="display:none"></div>
+		<div class="response" id="mce-success-response" style="display:none"></div>
+	</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_6da71990ae231989fb097187b_a1f732c8eb" tabindex="-1" value=""></div>
+    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button" style="display:none"></div>
+    </div>
+</form>
+</div>
+
+<!--End mc_embed_signup-->
 <!--     <label>Join the Mailinglist:</label><input disabled placeholder="email" />
  -->  </div>
 </template>
